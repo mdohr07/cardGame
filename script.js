ea76158f579e6c42cd5d3a525f818a9cb3aa8320
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2025-03-31 14:05:41 +0200

    Add enemy cards

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 // Canvas einrichten
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-const cards = [];
 const cardWidth = 120;
 const cardHeight = 170;
 
+const playerCards = [];
+const enemyCards = [];
+
 // JSON-Datei laden
 fetch("cards.json")
-    .then(response => response.json()) // Hier wird das JSON in ein Array umgewandelt
+    .then(response => response.json())
     .then(data => {
-let loadedImages = 0;
+        let loadedImages = 0;
 
+        // Spielerkarten
         data.forEach((cardData, index) => {
             const imgFront = new Image();
             const imgBack = new Image();
-        
+
             imgFront.src = `img/${cardData.imgFront}`;
             imgBack.src = `img/${cardData.imgBack}`;
 
             imgFront.onload = imgBack.onload = () => {
-                loadedImages++;
-                if (loadedImages === data.length * 2) {
+                loadedImages++; 
+                if (loadedImages === data.length * 2) { 
                     drawCards(); // Zeichne erst, wenn ALLE Bilder geladen sind
                 }
             };
 
-            cards.push({ // Cards-Array befüllen
+            playerCards.push({ // Cards-Array befüllen
                 cardName: cardData.cardName,
                 cardNumber: cardData.cardNumber,
                 atck: cardData.atck,
                 dfns: cardData.dfns,
-                x: index * (canvas.width / data.length), // teilt die Gesamtbreite des Canvas in gleich große Abschnitte
+                x: index * (canvas.width / data.length), // Gleichmäßige Verteilung
                 y: canvas.height - cardHeight - 10, // Am unteren Rand positionieren
                 width: cardWidth,
                 height: cardHeight,
@@ -39,48 +42,131 @@ let loadedImages = 0;
                 imgBack: imgBack
             });
         });
+
+        // Gegnerkarten
+        data.forEach((cardData, index) => {
+            const imgFront = new Image();
+            const imgBack = new Image();
+
+            imgFront.src = `img/${cardData.imgFront}`;
+            imgBack.src = `img/${cardData.imgBack}`;
+
+            enemyCards.push({
+                cardName: cardData.cardName,
+                cardNumber: cardData.cardNumber,
+                atck: cardData.atck,
+                dfns: cardData.dfns,
+                x: index * (canvas.width / data.length), // Gleichmäßige Verteilung
+                y: 20, // Am oberen Rand
+                width: cardWidth,
+                height: cardHeight,
+                isFlipped: false,
+                imgFront: imgFront,
+                imgBack: imgBack
+            });
+        });
+
+        drawCards();
     })
     .catch(error => console.error("Something went wrong when loading the JSON-file:", error));
 
-    // Zeichnet alle Karten
-    function drawCards() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        cards.forEach(card => {
-            const img = card.isFlipped ? card.imgFront : card.imgBack;
-            ctx.drawImage(img, card.x, card.y, card.width, card.height);
+// Zeichnet alle Karten (Spieler und Gegner)
+function drawCards() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            if (card.isFlipped) {
-                ctx.fillStyle = "white";
-                ctx.strokeStyle = "black";
-                ctx.font = "14px Arial";
-                ctx.textAlign = "center"; 
-                ctx.shadowColor = "black";  
-                ctx.shadowOffsetX = 1;      
-                ctx.shadowOffsetY = 1;      
-                ctx.shadowBlur = 3; 
-                ctx.fillText(`${card.cardName}`, card.x + card.width / 2, card.y + 20);
-                ctx.textAlign = "right";
-                ctx.fillText(`${card.atck}/${card.dfns}`, card.x + 110, card.y + card.height - 10);
-            }
-        });
-    }
+    // Zeichne alle Spielerkarten
+    playerCards.forEach(card => {
+        const img = card.isFlipped ? card.imgFront : card.imgBack;
+        ctx.drawImage(img, card.x, card.y, card.width, card.height);
 
-    // Click-Event: turn cards around
-    canvas.addEventListener("click", (event) => {
-        const rect = canvas.getBoundingClientRect();
-        const mouseX = event.clientX - rect.left;
-        const mouseY = event.clientY - rect.top;
-
-        cards.forEach(card => { // Jede Karte prüfen ob sie angeklickt wurde
-            if (
-                mouseX >= card.x && mouseX <= card.x + card.width && // War die Maus innerhalb der Kartenkoordinaten?
-                mouseY >= card.y && mouseY && mouseY <= card.y + card.height // Falls ja, wird isFlipped geändert ↓
-            ) {
-                card.isFlipped = !card.isFlipped;
-            }
-        });
-        drawCards();
+        if (card.isFlipped) {
+            ctx.fillStyle = "white";
+            ctx.strokeStyle = "black";
+            ctx.font = "14px Arial";
+            ctx.textAlign = "center";
+            ctx.shadowColor = "black";
+            ctx.shadowOffsetX = 1;
+            ctx.shadowOffsetY = 1;
+            ctx.shadowBlur = 3;
+            ctx.fillText(`${card.cardName}`, card.x + card.width / 2, card.y + 20);
+            ctx.textAlign = "right";
+            ctx.fillText(`${card.atck}/${card.dfns}`, card.x + 110, card.y + card.height - 10);
+        }
     });
+
+    // Zeichne alle Gegnerkarten
+    enemyCards.forEach(card => {
+        const img = card.isFlipped ? card.imgFront : card.imgBack;
+        ctx.drawImage(img, card.x, card.y, card.width, card.height);
+
+        if (card.isFlipped) {
+            ctx.fillStyle = "white";
+            ctx.strokeStyle = "black";
+            ctx.font = "14px Arial";
+            ctx.textAlign = "center";
+            ctx.shadowColor = "black";
+            ctx.shadowOffsetX = 1;
+            ctx.shadowOffsetY = 1;
+            ctx.shadowBlur = 3;
+            ctx.fillText(`${card.cardName}`, card.x + card.width / 2, card.y + 20);
+            ctx.textAlign = "right";
+            ctx.fillText(`${card.atck}/${card.dfns}`, card.x + 110, card.y + card.height - 10);
+        }
+    });
+}
+
+// Klick-Event: Karte umdrehen
+canvas.addEventListener("click", (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    playerCards.forEach(card => {
+        if (
+            mouseX >= card.x && mouseX <= card.x + card.width &&
+            mouseY >= card.y && mouseY <= card.y + card.height
+        ) {
+            card.isFlipped = !card.isFlipped;
+        }
+    });
+
+    enemyCards.forEach(card => {
+        if (
+            mouseX >= card.x && mouseX <= card.x + card.width &&
+            mouseY >= card.y && mouseY <= card.y + card.height
+        ) {
+            card.isFlipped = !card.isFlipped;
+        }
+    });
+
+    drawCards(); // Aktualisiere das Canvas nach jedem Klick
+});
+
+
+        
+
+// function drawCards() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     cards.forEach(card => {
+//         const img = card.isFlipped ? card.imgFront : card.imgBack;
+//         ctx.drawImage(img, card.x, card.y, card.width, card.height);
+
+//         if (card.isFlipped) {
+//             ctx.fillStyle = "white";
+//             ctx.strokeStyle = "black";
+//             ctx.font = "14px Arial";
+//             ctx.textAlign = "center";
+//             ctx.shadowColor = "black";
+//             ctx.shadowOffsetX = 1;
+//             ctx.shadowOffsetY = 1;
+//             ctx.shadowBlur = 3;
+//             ctx.fillText(`${card.cardName}`, card.x + card.width / 2, card.y + 20);
+//             ctx.textAlign = "right";
+//             ctx.fillText(`${card.atck}/${card.dfns}`, card.x + 110, card.y + card.height - 10);
+//         }
+//     });
+// }
+
 
 // const cardBack = new Image();
 // const cardFront = new Image();
